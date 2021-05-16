12c47bc75d4607ad571b592f77dcadecf7ed20aa
AndreaRoverso <81749003+AndreaRoverso@users.noreply.github.com>
2021-05-16 12:40:04 +0200

    Update R_code_classification.r

--- R_code_classification.r
+++ R_code_classification.r
@@ -26,3 +26,13 @@ plot(sunc$map)
 
 gc <- brick("dolansprings_oli_2013088_canyon_lrg.jpg")
 plotRGB(gc, r=1, g=2, b=3, stretch="lin")
+plotRGB(gc, r=1, g=2, b=3, stretch="hist")
+
+gcc2 <- unsuperClass(gc, nClasses=2)
+gcc2
+plot(gcc2$map)
+
+
+gcc4 <- unsuperClass(gc, nClasses=4)
+plot(gcc4$map)
+
