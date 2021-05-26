c78aa05b57ab354012c29ae2bc596db6383ff390
AndreaRoverso <81749003+AndreaRoverso@users.noreply.github.com>
2021-05-26 16:29:56 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -1,2 +1,27 @@
 #R_code_variability.r
 
+library(raster)
+library(RStoolbox) 
+library(ggplot2)
+#install.packages("viridis")
+library(viridis)
+library(gridExtra)
+setwd("C:/lab/") 
+
+sent <- brick("sentinel.png")
+
+# NIR 1, RED 2, GREEN 3
+plotRGB(sent, r=2, g=1, b=3, stretch="lin")
+
+nir <- sent$sentinel.1
+red <- sent$sentinel.2
+
+ndvi <- (nir-red)/(nir+red)
+plot(ndvi)
+cl <- colorRampPalette(c("black","white","red","magenta","green"))(100)
+plot(ndvi,col=cl)
+
+ndvisd3 <- focal(ndvi, w=matrix(1/9, nrow=3, ncol=3, fun=sd)
+plot(ndvisd3)
+
+                
