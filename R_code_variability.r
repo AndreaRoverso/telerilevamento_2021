172db92b780e376bda6fd357873e94a634744d5c
AndreaRoverso <81749003+AndreaRoverso@users.noreply.github.com>
2021-08-24 17:00:08 +0200

    Update R_code_variability.r

--- R_code_variability.r
+++ R_code_variability.r
@@ -1,23 +1,34 @@
 #R_code_variability.r
 
+#librerie
+
 library(raster)
 library(RStoolbox) 
 library(ggplot2)
 #install.packages("viridis")
 library(viridis)
 library(gridExtra)
-setwd("C:/lab/") 
 
+setwd("C:/lab/")
+
+#importo immagine con la funzione brick
 sent <- brick("sentinel.png")
 
-# NIR 1, RED 2, GREEN 3
+# NIR 1, RED 2 , GREEN 3
+# r=1, g=2, B=3
+plotRGB(sent, stretch="lin") #composizone di default
+# oppure: 
 plotRGB(sent, r=2, g=1, b=3, stretch="lin")
 
+#associo 1° banda a oggetto (nir)
 nir <- sent$sentinel.1
+#associo 2° banda a oggetto (red)
 red <- sent$sentinel.2
 
+#calcolo NDVI e plot
 ndvi <- (nir-red)/(nir+red)
 plot(ndvi)
+#cambio colori con la colorRampPalette
 cl <- colorRampPalette(c("black","white","red","magenta","green"))(100)
 plot(ndvi,col=cl)
 
