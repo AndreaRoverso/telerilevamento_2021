4fa84a8ff093dc1ebc529f2d9062b57055b175a6
AndreaRoverso <81749003+AndreaRoverso@users.noreply.github.com>
2021-09-07 17:07:31 +0200

    Update R_code_exam_project.r

--- R_code_exam_project.r
+++ R_code_exam_project.r
@@ -3,7 +3,7 @@
 # Shinking Aral Sea from 2000 to 2018 
 # Images from: https://earthobservatory.nasa.gov/world-of-change/AralSea
 
-# library
+# Library
 library(raster)
 library(rasterVis)
 library(RStoolbox)
@@ -13,61 +13,40 @@ library(rgdal)
 
 setwd("C:/lab/exam") 
 
-# importo i file 
-# creo una lista di file con list.files per la funzione lapply 
-# arallist <- list.files(pattern="aralsea") 
-# arallist
-
-# applico con lapply una funzione raster alla lista dei file
-# import <- lapply(arallist,raster)
-# import
-# uso stack che mi raggruppa i singoli files in uno unico
-# TGr <- stack(import)
-# plot(TGr)
-# plotto con immagini sovrapposte
-# plotRGB(TGr, r=1, g=2, b=3, stretch="lin")
-# plotto in scale di grigio diverse, coordinate x e y e qualità immagine superiore
-# ggRGB(TGr, r=1, g=2, b=3, stretch="lin")
-
-
+# Funzione brick per le 4 immagini in tre bande
 arals2000 <- brick("aralsea2000.jpg")
 arals2006 <- brick("aralsea2006.jpg")
 arals2012 <- brick("aralsea2012.jpg")
 arals2018 <- brick("aralsea2018.jpg")
 
-# Funzione plotRGB
+# Funzione plotRGB per plottare con i colori originali
 par(mfrow=c(2,2))
 plotRGB(arals2000, r=1, g=2, b=3, stretch="lin") 
 plotRGB(arals2006, r=1, g=2, b=3, stretch="lin")
 plotRGB(arals2012, r=1, g=2, b=3, stretch="lin") 
 plotRGB(arals2018, r=1, g=2, b=3, stretch="lin") 
 
-# Upload with function raster
+# Funzione raster per importare le immagini con una sola banda
 aral2000r <- raster("aralsea2000.jpg")
 aral2006r <- raster("aralsea2006.jpg")
 aral2012r <- raster("aralsea2012.jpg")
 aral2018r <- raster("aralsea2018.jpg") 
 
-
+# Scelgo i colori
 col <- colorRampPalette(c("green","yellow","pink","blue"))(100)
 
+# Creo uno stack 
 aralsea <- stack(aral2000r,aral2006r,aral2012r,aral2018r) 
 
+# Plot con levelPlot che disegna più grafici di colore "falso" con una singola legenda
+# Con names.attr=c do uno titolo diverso alle quattro immagini
 levelplot(aralsea,col.regions=col, main="LAGO D'ARAL", names.attr=c("Situazione nel 2000","Situazione nel 2006","Situazione nel 2012","Situazione nel 2018"))
 
 
-# Funzione levelplot: disegna più grafici di colore falso con una singola legenda
-# levelplot(TGr)
-# colorRampPalette (NON SI USA SU RGB)
-# col2<-colorRampPalette(c("white","dark green","yellow","red","black"))(100)
-
-# Nuovo levelplot col cambio di colori, nome e titolo
-# levelplot(TGr,col.regions=col, main="Shinking Aral Sea", names.attr=c("2000","2006", "2012", "2018"))
-
 ##########################################################
 
-# Plot di tutte le correlazioni tra bande di un dataset (matrice di scatterplot di dati, non immagini)
-# La tabella riporta in diagonale le bande (sono le variabili), l'indice di correlazione varia da 0(minima) a 1 (massima)
+# Plot di tutte le correlazioni tra bande di un dataset 
+# Il grafico riporta in diagonale le bande (sono le variabili), l'indice di correlazione varia da 0( correlazione minima) a 1 (correlazione massima)
 pairs(aralsea, main="Comparazione con la funzione pairs")
 # Indice di correlazione: più le bande sono correlate e maggiore sarà la dimensione dei caratteri
 
@@ -87,9 +66,9 @@ summary(aralsea2000pca$model)
 # Cumulative Proportion   0.9570181  0.98164266  1.00000000
 
 plotRGB(aralsea2000pca$map,r=1,g=3,b=2, stretch="hist")
+
 # Per vedere il grafico
 plot(aralsea2000pca$model) 
-
 
 # PCA 2006
 aralsea2006pca <- rasterPCA(arals2006)
@@ -103,9 +82,9 @@ summary(aralsea2006pca$model)
 # Cumulative Proportion   0.9419963  0.99030439 1.000000000
 
 plotRGB(aralsea2006pca$map,r=1,g=3,b=2, stretch="hist")
+
 # Per vedere il grafico
 plot(aralsea2006pca$model) 
-
 
 # PCA 2012
 aralsea2012pca <- rasterPCA(arals2012)
@@ -119,10 +98,9 @@ summary(aralsea2012pca$model)
 # Cumulative Proportion   0.9453124  0.99257841 1.000000000
 
 plotRGB(aralsea2012pca$map,r=1,g=3,b=2, stretch="hist")
+
 # Per vedere il grafico
 plot(aralsea2012pca$model) 
-
-
 
 # PCA 2018
 aralsea2018pca <- rasterPCA(arals2018)
@@ -136,6 +114,7 @@ summary(aralsea2018pca$model)
 # Cumulative Proportion   0.9318901  0.98997563 1.00000000
 
 plotRGB(aralsea2018pca$map,r=1,g=3,b=2, stretch="hist")
+
 # Per vedere il grafico
 plot(aralsea2018pca$model) 
 
@@ -155,8 +134,7 @@ grid.arrange(as2000, as2006,as2012,as2018, nrow=2)
 
 C <- grid.arrange(as2000, as2006, as2012, as2018, nrow=2)
 ggsave("grid.arrange.jpg",C) 
-# bisogna specificare cosa salvare esplicitamente, altrimenti salva solo l'ultimo grafico non l'intero grid.arrange
-
+# Bisogna specificare cosa salvare esplicitamente, altrimenti salva solo l'ultimo grafico non l'intero grid.arrange
 
 
 
@@ -190,12 +168,13 @@ plot(spAs2018, col=cl)
 
 # Firme spettrali
 
+# Immagine del 2000
 plotRGB(arals2000, r=1, g=2, b=3, stretch="lin") 
-
-# Bisogna avere la mappa fatta con plotRGB aperta sotto
+# Bisogna avere la mappa con plotRGB aperta
 # Funzione: click, usa immagine plotRGB per creare le firme spettrali 
 click(arals2000, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 
+# Risultati:
 #      x     y  cell aralsea2000.1 aralsea2000.2 aralsea2000.3
 # 1 451.5 600.5 86132             2            19            11
 #      x     y   cell aralsea2000.1 aralsea2000.2 aralsea2000.3
@@ -203,9 +182,11 @@ click(arals2000, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 #      x     y   cell aralsea2000.1 aralsea2000.2 aralsea2000.3
 # 1 391.5 273.5 321512           101           127            56
 
+# Immagine del 2018
 plotRGB(arals2018, r=1, g=2, b=3, stretch="lin") 
 click(arals2018, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 
+# Risultati:
 #      x     y  cell aralsea2018.1 aralsea2018.2 aralsea2018.3
 # 1 460.5 591.5 92621            55           107            85
 #      x     y   cell aralsea2018.1 aralsea2018.2 aralsea2018.3
@@ -213,7 +194,7 @@ click(arals2018, id=T, xy=T, cell=T, type="p", pch=16, cex=4, col="yellow")
 #      x     y   cell aralsea2018.1 aralsea2018.2 aralsea2018.3
 # 1 157.5 223.5 357278           169           160           145
 
-# Definire le colonne del dataset
+# Creo un dataset con i risultati
 band <- c(1,2,3)
 aral2000p1 <- c(2,19,11)
 aral2000p2 <- c(5,58,32)
@@ -225,14 +206,13 @@ aral2018p3 <- c(169,160,145)
 spectralss <- data.frame(band,aral2000p1,aral2000p2,aral2000p3,aral2018p1,aral2018p2,aral2018p3)
 
 spectralss
-
 #  band   aral2000p1  aral2000p2  aral2000p3  aral2018p1  aral2018p2  aral2018p3
 #  1      2           5           101         55          162         169
 #  2      19          58          127         107         149         160
 #  3      11          32          56          85          130         145
 
 
-# Plot
+# Plotto con ggplot con un colore differente per ogni risultato
 ggplot(spectralss, aes(x=band)) +
 geom_line(aes(y = aral2000p1 ), color="red") +
 geom_line(aes(y = aral2000p2), color="green") +
